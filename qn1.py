2b10281e77f114163e17ae56358b8966b0aa3dd4
ReVuz <98228399+ReVuz@users.noreply.github.com>
2022-04-12 14:01:11 +0530

    Update qn1.py

--- qn1.py
+++ qn1.py
@@ -3,16 +3,18 @@ a female has produced another pair of rabbits. Suppose that our rabbits never di
 Develop a program to show a table containing the number of pairs of rabbits in the first N months.'''
 
 month = int(input("Enter the number of months : "))
-a,pair=0,0
-b=1
-print("Month \tPairs")
-for i in range(1,month+1):
-    if(i==1):
-        print(i,"\t",b)
-    else:
-        pair=a+b
-        a=b
-        b=pair
-        print(i,"\t",pair)
+def rabbits(x):
+	a,pair=0,0
+	b=1
+	print("Month \tPairs")
+	for i in range(1,month+1):
+		if(i==1):
+			print(i,"\t",b)
+		else:
+			pair=a+b
+			a=b
+			b=pair
 
+			print(i,"\t",pair)
+data=rabbits(month)
 
